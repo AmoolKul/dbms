0eb2ee07358fa26cdf58a436a16e7b78ed3168b1
Sarfaraz C <sarfuruuz@gmail.com>
2021-12-27 14:33:43 +0530

    pushing equipment page

---
+++ Escript.js
@@ -0,0 +1,43 @@
+// When the user clicks on div, open the popup
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+  }
+
+  // When the user clicks on div, open the popup
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+
+// When the user clicks on div, open the popup
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+  
+// When the user clicks on div, open the popup
+function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+  }
+
+// When the user clicks on div, open the popup
+function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+  }
+
+// When the user clicks on div, open the popup
+function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+  }
+  
+// When the user clicks on div, open the popup
+function myFunction7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+  }
+    
+  
